f91d7e35e0c2c1f6608d140bddb32ec7240744e0
terraform+1-at-067637015277 <onye71@gmail.com>
2023-08-14 11:57:52 -0400

    codebuild trigger

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Flask app'
+    return 'Hello, Flask app users, i just pushed my code to the github repo to trigger build'
